b1df1aed208fcfc70c91849fd1526ea4bf2278d6
prazzdev <prazzid31@gmail.com>
2023-03-17 15:38:30 +0700

    new file:   main.py 	new file:   test1/main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter
+
+window = tkinter.Tk()
+
+Label = tkinter.Label(window, text="Ichiro Hideyoshi")
+
+Label.pack()
+
+window.mainloop()
---
+++ test1/main.py
@@ -0,0 +1,17 @@
+import tkinter
+
+window = tkinter.Tk()
+
+Label1 = tkinter.Label(window, text="Ryoichi Hideyoshi")
+Label2 = tkinter.Label(window, text="Ichiro Hideyoshi")
+Label3 = tkinter.Label(window, text="Makoto Shinkai")
+Label4 = tkinter.Label(window, text="Sinichi Edogawa")
+Label5 = tkinter.Label(window, text="Conan Kudo")
+
+Label1.pack()
+Label2.pack()
+Label3.pack()
+Label4.pack()
+Label5.pack()
+
+window.mainloop()
